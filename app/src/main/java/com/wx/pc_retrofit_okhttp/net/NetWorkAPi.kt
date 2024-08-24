10a95871f4ccdc96cae915681d357003fb1ff891
wgllss <283495283@qq.com>
2024-08-24 21:49:37 +0800

    测试 写法

--- app/src/main/java/com/wx/pc_retrofit_okhttp/net/NetWorkAPi.kt
+++ app/src/main/java/com/wx/pc_retrofit_okhttp/net/NetWorkAPi.kt
@@ -1,15 +1,13 @@
 package com.wx.pc_retrofit_okhttp.net
 
-import com.wx.pc_retrofit_okhttp.data.CommonData
 import com.wx.pc_retrofit_okhttp.data.HomeData
-import com.wx.pc_retrofit_okhttp.data.WanAndroidHome
-import retrofit2.http.Field
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import okhttp3.RequestBody
+import retrofit2.http.*
 
 interface NetWorkAPi {
 
+    //示例post 请求
+    @FormUrlEncoded
     @POST("https://www.wanandroid.com/user/register")
     suspend fun register(
         @Field("username") username: String,
@@ -17,9 +15,24 @@ interface NetWorkAPi {
         @Field("repassword") repassword: String
     ): String
 
+    // 示例get 请求
     @GET("article/list/0/json")
     suspend fun getHomeList(): String
 
+    // 示例get 请求2
     @GET("article/list/{path}/json")
     suspend fun getHomeList22(@Path("path") page: Int): HomeData
+
+    /************************* 以下只 示例写法，接口调不通，因为找不到那么多 公开接口  全是 Retrofit的用法 来测试 *****************************************************/
+
+
+//    @FormUrlEncoded
+    @Headers("Content-Type: application/x-www-form-urlencoded")  //todo 固定 header
+    @POST("https://xxxxxxx")
+    fun post1(@Body body: RequestBody): String
+
+//    @FormUrlEncoded
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @POST("https://xxxxxxx22222")
+    fun post12(@Body body: RequestBody, @HeaderMap map: Map<String, String>): String //todo  HeaderMap 多个请求头部自己填写
 }
--- app/src/main/java/com/wx/pc_retrofit_okhttp/repository/WXRepository.kt
+++ app/src/main/java/com/wx/pc_retrofit_okhttp/repository/WXRepository.kt
@@ -1,6 +1,8 @@
 package com.wx.pc_retrofit_okhttp.repository
 
+import com.google.gson.Gson
 import com.wx.pc_retrofit_okhttp.net.NetWorkAPi
+import com.wx.pc_retrofit_okhttp.net.retrofit.RequestBodyWrapper
 import com.wx.pc_retrofit_okhttp.net.retrofit.RetrofitUtils
 import com.wx.pc_retrofit_okhttp.parseErrorString
 import kotlinx.coroutines.Dispatchers
@@ -10,6 +12,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.runBlocking
+import retrofit2.http.Field
 
 class WXRepository private constructor() {
 
@@ -46,9 +49,68 @@ class WXRepository private constructor() {
                 it.data?.datas?.forEachIndexed { i, it ->
                     println("第$i 条 : ${it.title}") // 打印异常信息
                 }
-
             }
-            delay(10000) // 让程序主进程 在打印前不结束
+            delay(10000) // 让java工程 程序主进程 在打印前不结束
+        }
+    }
+
+    fun register(username: String, password: String, repassword: String) {
+        runBlocking {
+            flow {
+                emit(api.register(username, password, repassword))
+            }.flowOn(Dispatchers.IO).catch {
+                println(it)
+                println(it.parseErrorString()) // 打印异常信息
+            }.collect {
+                println(it)
+            }
+        }
+    }
+
+    /************************* 以下只 示例写法，接口调不通，因为找不到那么多 公开接口  全是 Retrofit的用法 来测试 *****************************************************/
+
+    fun post1() {
+        runBlocking {
+            flow {
+                val map = mutableMapOf<String, Any>()
+                map["AAAAAA"] = "AAAAAA"
+                map["BBBBBB"] = 1
+                map["CCCCCC"] = true
+                map["DDDDDD"] = 30.00f
+                emit(api.post1(RequestBodyWrapper(Gson().toJson(map))))
+            }.flowOn(Dispatchers.IO).catch {
+                println(it)
+                println(it.parseErrorString()) // 打印异常信息
+            }.collect {
+                println(it)
+            }
+        }
+    }
+
+
+    fun post12() {
+        runBlocking {
+            flow {
+                val map = mutableMapOf<String, Any>()
+                map["AAAAAA"] = "AAAAAA"
+                map["BBBBBB"] = 1
+                map["CCCCCC"] = true
+                map["DDDDDD"] = 30.00f
+
+                val headerMap = mapOf(
+                    "Cookie" to "uab_collina=171755326751237298573402; tfstk=fFqEGyx3iMIEHdYy8fir_5GQozgKVDffUuGSE82odXcnd0awZ5V9FXiIAPlzs5lhp6CLa3Van4c7V09LWSwDAk_KV3-K20ffGisXpJn-qHga3LOpSxMhVYcnqYuGorffGis_pJn-qsa52bcgOYhMKYc3Zh0i6Y8oqX0ksFDmsbmoq7xGjxM2xU0oKdc9M402bY3h9NU_OKjk2plbKf-2BPkMqjsxT3Y7bvRjZJYDq3qZL2ESMIWRh2auHqEQQg-tA-zijYrRnUlruzqIjkfGmV3uz7DTW_8rgz2QkAqcFEh_bXNuYVXX04igcqm35NCIPDkoxqUOeeGur-rqAW19qXa4I5ubfCtjmrFa4qmc4XO-ISE82yRkr2xSQj6NI96RN__qDt2vyU3B6AlfK9YkrIKXVer5pUL-RaDZG9XA.; uag=7aed8d4dcb46b3eaa0e85f86b21df22e; ylogin=4024975; folder_id_c=10376791; phpdisk_info=WGhSYARkBz8BOgRjDmEEV1QwAAsAaAVlAzcDYw8xCjlTZFdjUjcDPFJiUAkMYFU9VD0NP109BWIPbgQxATMKPVg5UjUEMgdtAWAENQ5jBG5UZgA6AGoFawM4A2oPPwptU2FXYlJiAzxSZ1BhDF9VPlQzDThdNAVqDz0EZwExCj9Yb1Ji; PHPSESSID=pbba5e4111h3fk7om64mdgf4sbd0c6lm; __tins__21412745=%7B%22sid%22%3A%201717724688543%2C%20%22vd%22%3A%2012%2C%20%22expires%22%3A%201717727331949%7D; __51cke__=; __51laig__=12",
+                    "Host" to "up.SSSSSwxxxoozooo.com",
+                    "Origin" to "http://AAAAAAAAup.wxssosdsozosdoo.com",
+                    "Referer" to "http://AAAAAAAup.wodsdsdsdsdosddszooo.com/mydisk.php?item=files&action=index&u=4024975"
+                )
+
+                emit(api.post12(RequestBodyWrapper(Gson().toJson(map)), headerMap))
+            }.flowOn(Dispatchers.IO).catch {
+                println(it)
+                println(it.parseErrorString()) // 打印异常信息
+            }.collect {
+                println(it)
+            }
         }
     }
 }
