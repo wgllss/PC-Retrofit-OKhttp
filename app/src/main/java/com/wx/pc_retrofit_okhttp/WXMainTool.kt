68cb73f8111707f625f4b9502fee789c1ff55854
wgllss <283495283@qq.com>
2024-08-26 08:48:31 +0800

    测试 写法 11

--- app/src/main/java/com/wx/pc_retrofit_okhttp/WXMainTool.kt
+++ app/src/main/java/com/wx/pc_retrofit_okhttp/WXMainTool.kt
@@ -5,7 +5,7 @@ import com.wx.pc_retrofit_okhttp.repository.WXRepository
 
 fun main() {
 //    WXRepository.instance.getHomeList()
-//    WXRepository.instance.getHomeList22()
-    WXRepository.instance.register("WXXXXXXXXXX","1212121","1212121")
+    WXRepository.instance.getHomeList22()
+//    WXRepository.instance.register("WXXXXXXXXXX","1212121","1212121")
 
 }
